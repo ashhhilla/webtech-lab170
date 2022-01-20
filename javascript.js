0dea0b0644411787cbf6987e753d4c9d9a0f0696
Ashilla Fryda Risautami <93770101+ashhhilla@users.noreply.github.com>
2022-01-20 14:45:25 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -29,8 +29,16 @@ document.querySelectorAll(".tableT th").forEach(headerCell => {
     headerCell.addEventListener("click", () => {
         const tableElement = headerCell.parentElement.parentElement.parentElement;
         const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
-        const currentIsAscending = headerCell.classList.contains("th-sort-asc");
+       const currentIsAscending = headerCell.classList.contains("th-sort-asc");
+        sortTableByColumn(tableElement, headerIndex, !currentIsAscending);
+    });
+});
 
+document.querySelectorAll(".PhoneT th").forEach(headerCell => {
+    headerCell.addEventListener("click", () => {
+        const tableElement = headerCell.parentElement.parentElement.parentElement;
+        const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
+       const currentIsAscending = headerCell.classList.contains("th-sort-asc");
         sortTableByColumn(tableElement, headerIndex, !currentIsAscending);
     });
 });
